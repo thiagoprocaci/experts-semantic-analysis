25373f2435bd4f220273a9a14b54953045716966
tbp <thiagoprocaci@gmail.com>
2016-01-17 20:12:11 -0200

    table V

---
+++ table-V-recommendation/sqls/humans.sql
@@ -0,0 +1,301 @@
+select userProfile.*, p.votosPositivos, p.votosNegativos from (
+
+
+SELECT *
+FROM (
+SELECT 
+	a.id_text AS idText,
+	a.tipo AS tipo,
+	a.score AS score,
+	e.uri AS uri,
+	e.surface AS surface,
+	(
+SELECT p.usuarioID
+FROM pergunta p
+WHERE p.id = a.id_text) AS idUsuario,
+	(
+SELECT 'BIO'
+FROM DUAL) AS comunidade
+FROM anotacoes a
+INNER JOIN entidades e ON a.id_entidade = e.id
+WHERE a.id_text LIKE '1_%' AND (a.tipo = 'titulo_pergunta' OR a.tipo = 'texto_pergunta') AND a.id_text IN (
+SELECT p.id
+FROM pergunta p
+WHERE p.usuarioID IN (
+'1_3632',
+'1_1320',
+'1_3340',
+'1_5144',
+'1_4101',
+'1_1306',
+'1_1231',
+'1_1757',
+'1_560',
+'1_4108',
+'1_107',
+'1_806',
+'1_115',
+'1_1122',
+'1_69',
+'1_1590',
+'1_535',
+'1_948',
+'1_66',
+'1_3624',
+'1_389',
+'1_3952',
+'1_740',
+'1_3971',
+'1_6',
+'1_112',
+'1_3059',
+'1_28',
+'1_444',
+'1_430',
+'1_4168',
+'1_86',
+'1_3615',
+'1_4579',
+'1_2938',
+'1_1441',
+'1_43',
+'1_166',
+'1_5754',
+'1_57',
+'1_5148',
+'1_3776',
+'1_231',
+'1_307',
+'1_4177',
+'1_81',
+'1_67',
+'1_3460',
+'1_5694',
+'1_3053'
+)) UNION ALL
+SELECT 
+	a.id_text AS idText,
+	a.tipo AS tipo,
+	a.score AS score,
+	e.uri AS uri,
+	e.surface AS surface,
+	(
+SELECT p.usuarioID
+FROM resposta p
+WHERE p.id = a.id_text) AS idUsuario,
+	(
+SELECT 'BIO'
+FROM DUAL) AS comunidade
+FROM anotacoes a
+INNER JOIN entidades e ON a.id_entidade = e.id
+WHERE a.id_text LIKE '1_%' AND (a.tipo = 'texto_resposta') AND a.id_text IN (
+SELECT p.id
+FROM resposta p
+WHERE p.usuarioID IN (
+'1_3632',
+'1_1320',
+'1_3340',
+'1_5144',
+'1_4101',
+'1_1306',
+'1_1231',
+'1_1757',
+'1_560',
+'1_4108',
+'1_107',
+'1_806',
+'1_115',
+'1_1122',
+'1_69',
+'1_1590',
+'1_535',
+'1_948',
+'1_66',
+'1_3624',
+'1_389',
+'1_3952',
+'1_740',
+'1_3971',
+'1_6',
+'1_112',
+'1_3059',
+'1_28',
+'1_444',
+'1_430',
+'1_4168',
+'1_86',
+'1_3615',
+'1_4579',
+'1_2938',
+'1_1441',
+'1_43',
+'1_166',
+'1_5754',
+'1_57',
+'1_5148',
+'1_3776',
+'1_231',
+'1_307',
+'1_4177',
+'1_81',
+'1_67',
+'1_3460',
+'1_5694',
+'1_3053'
+)) 
+
+
+) A ) userProfile, pergunta p where lower(userProfile.surface) = 'humans'
+		and p.id = userProfile.idText
+
+union all
+
+select userProfile.*, p.votosPositivos, p.votosNegativos from (
+
+
+SELECT *
+FROM (
+SELECT 
+	a.id_text AS idText,
+	a.tipo AS tipo,
+	a.score AS score,
+	e.uri AS uri,
+	e.surface AS surface,
+	(
+SELECT p.usuarioID
+FROM pergunta p
+WHERE p.id = a.id_text) AS idUsuario,
+	(
+SELECT 'BIO'
+FROM DUAL) AS comunidade
+FROM anotacoes a
+INNER JOIN entidades e ON a.id_entidade = e.id
+WHERE a.id_text LIKE '1_%' AND (a.tipo = 'titulo_pergunta' OR a.tipo = 'texto_pergunta') AND a.id_text IN (
+SELECT p.id
+FROM pergunta p
+WHERE p.usuarioID IN (
+'1_3632',
+'1_1320',
+'1_3340',
+'1_5144',
+'1_4101',
+'1_1306',
+'1_1231',
+'1_1757',
+'1_560',
+'1_4108',
+'1_107',
+'1_806',
+'1_115',
+'1_1122',
+'1_69',
+'1_1590',
+'1_535',
+'1_948',
+'1_66',
+'1_3624',
+'1_389',
+'1_3952',
+'1_740',
+'1_3971',
+'1_6',
+'1_112',
+'1_3059',
+'1_28',
+'1_444',
+'1_430',
+'1_4168',
+'1_86',
+'1_3615',
+'1_4579',
+'1_2938',
+'1_1441',
+'1_43',
+'1_166',
+'1_5754',
+'1_57',
+'1_5148',
+'1_3776',
+'1_231',
+'1_307',
+'1_4177',
+'1_81',
+'1_67',
+'1_3460',
+'1_5694',
+'1_3053'
+)) UNION ALL
+SELECT 
+	a.id_text AS idText,
+	a.tipo AS tipo,
+	a.score AS score,
+	e.uri AS uri,
+	e.surface AS surface,
+	(
+SELECT p.usuarioID
+FROM resposta p
+WHERE p.id = a.id_text) AS idUsuario,
+	(
+SELECT 'BIO'
+FROM DUAL) AS comunidade
+FROM anotacoes a
+INNER JOIN entidades e ON a.id_entidade = e.id
+WHERE a.id_text LIKE '1_%' AND (a.tipo = 'texto_resposta') AND a.id_text IN (
+SELECT p.id
+FROM resposta p
+WHERE p.usuarioID IN (
+'1_3632',
+'1_1320',
+'1_3340',
+'1_5144',
+'1_4101',
+'1_1306',
+'1_1231',
+'1_1757',
+'1_560',
+'1_4108',
+'1_107',
+'1_806',
+'1_115',
+'1_1122',
+'1_69',
+'1_1590',
+'1_535',
+'1_948',
+'1_66',
+'1_3624',
+'1_389',
+'1_3952',
+'1_740',
+'1_3971',
+'1_6',
+'1_112',
+'1_3059',
+'1_28',
+'1_444',
+'1_430',
+'1_4168',
+'1_86',
+'1_3615',
+'1_4579',
+'1_2938',
+'1_1441',
+'1_43',
+'1_166',
+'1_5754',
+'1_57',
+'1_5148',
+'1_3776',
+'1_231',
+'1_307',
+'1_4177',
+'1_81',
+'1_67',
+'1_3460',
+'1_5694',
+'1_3053'
+)) 
+
+
+) A ) userProfile, resposta p where lower(userProfile.surface) = 'humans'
+		and p.id = userProfile.idText
---
+++ table-V-recommendation/table-V.R
@@ -0,0 +1,71 @@
+# Vargha & Delaney's A12
+vargha.delaney <- function(r1, r2) {
+  m <- length(r1);
+  n <- length(r2);
+  return ((sum(rank(c(r1, r2))[seq_along(r1)]) / m - (m + 1) / 2) / n);
+}
+
+
+rootDir <- "data"
+initialFiles <- dir(rootDir)
+
+entityList <- c()
+for (f in initialFiles) {
+  filePath <- paste(rootDir,f,sep = "/")
+  data <- read.csv(filePath, header=TRUE, sep = ';')
+  surface <- unique(as.character(data$surface))
+  entityList <- c(entityList, surface)
+}
+
+
+col <- c("VOTE_UP_MEAN", "VOTE_DOWN_MEAN", "PV", "EFFECT_SIZE", "NUMBER_USER")
+
+numLines <- length(entityList)
+numCol <- length(col)
+
+
+tableResult <- matrix(nrow=numLines, ncol=numCol, dimnames=list(entityList,col))
+
+for (f in initialFiles) {
+  filePath <- paste(rootDir,f,sep = "/")
+  data <- read.csv(filePath, header=TRUE, sep = ';')
+  surface <- unique(as.character(data$surface))
+  voteUpList <- as.numeric(data$votosPositivos)
+  voteDownList <- as.numeric(data$votosNegativos)
+  users <- unique(as.character(data$idUsuario))
+  
+  tableResult[surface, "NUMBER_USER"] <- length(users)
+  tableResult[surface, "VOTE_UP_MEAN"] <- paste(format(mean(voteUpList), nsmall = 4, digits = 4), sep = "+-", format(round(sd(voteUpList), digits = 2), nsmall = 2))
+  tableResult[surface, "VOTE_DOWN_MEAN"] <- paste(format(mean(voteDownList), nsmall = 4, digits = 4), sep = "+-", format(round(sd(voteDownList), digits = 2), nsmall = 2))
+  tableResult[surface, "EFFECT_SIZE"] <- paste(round(vargha.delaney(voteUpList,voteDownList) * 100), sep = "", "%")
+  pv <- wilcox.test(voteUpList,voteDownList)$p.value
+  
+  if(is.nan(pv)) { #as vezes da NaN!
+    tableResult[surface, "PV"]  <- '-'
+    next
+  }
+  if(pv < 0.001) {
+    tableResult[surface, "PV"]  <- '< 0,001'
+    next
+  } 
+  tableResult[surface, "PV"]  <- as.character(format(round(pv, digits = 4), nsmall = 4))
+  
+}
+
+print(tableResult)
+#write.table(tableResult, file="profileEval2.csv", sep=";")
+
+#config_original = par(no.readonly = TRUE)
+#par(mfrow = c(4,3), pty = "s", mar = c(2, 2, 2, 2))
+
+#for (f in initialFiles) {
+#  filePath <- paste(rootDir,f,sep = "/")
+#  data <- read.csv(filePath, header=TRUE, sep = ';')
+#  surface <- unique(as.character(data$surface))
+#  voteUpList <- as.numeric(data$votosPositivos)
+#  voteDownList <- as.numeric(data$votosNegativos)
+
+#boxplot(voteUpList, voteDownList, names = c("vote up", "vote down"), main  = surface, ylab = "Evaluations")
+#}
+#par(config_original)
+
